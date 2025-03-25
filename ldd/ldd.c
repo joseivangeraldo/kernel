a8ff37d9685c15e286cfaf6c06189ec0b88a1513
José Ivan Geraldo da Silva <67489053+joseivangeraldo@users.noreply.github.com>
2025-03-25 23:56:56 +0000

    2025-03-25 23:56:53 ' Acrescentado'

--- ldd/ldd.c
+++ ldd/ldd.c
@@ -1,20 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-MODULE_LICENCE("GPL"); //*
-MODULE_AUTHOR("InPyjama - Piyush"); //!
-MODULE_DESCRIPTION("Our first dynamically loadable kernel module"); //!
+MODULE_LICENSE("GPL");
 
-static int pyjama_module_init (void){
-    printk("Hello, World! (from InPyjama!!)
-\n");
+static int __init hello_init(void)
+{
+    printk(KERN_ALERT "Hello world\n");
     return 0;
 }
 
-static void pyjama_module_exit (void) {
-    printk("Good, Bye! (from InPyjama!!)
-\n");
+static void __exit hello_exit(void)
+{
+    printk(KERN_ALERT "Goodbye\n");
 }
 
-module_init(pyjama_module_init);
-module_exit(pyjama_module_exit);
+module_init(hello_init);
+module_exit(hello_exit);
