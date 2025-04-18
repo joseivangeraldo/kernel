57821d937dc7f84693dd645b47124f5a9d27f01a
José Ivan Geraldo Silva <ivan.geraldo@gmail.com>
2025-04-18 15:54:25 +0000

    'Alterado em '18-04-2025 15:54:22

---
+++ makefiles/ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+extern int util(int);
+
+int main(int argc, char **argv)
+{
+    return util(argc);
+}
---
+++ makefiles/util2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int util(int argc)
+{
+    printf("argc %d\n", argc);
+    return 0;
+}
